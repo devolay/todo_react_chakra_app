8411cd7e83a320b5038b3106c34cb3b154cf1c75
dawsta006 <dawsta006@o365.student.utp.edu.pl>
2021-06-18 20:06:16 +0200

    adding icon and working on heading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Heading } from '@chakra-ui/react'
+import TodoList from './components/TodoList'
+import AddTodo from './components/AddTodo'
+import { VStack, IconButton } from '@chakra-ui/react'
+import { FaSun, FaMoon } from 'react-icons/fa'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack p={4}>
+      <IconButton icon={<FaSun></FaSun>} isRound='true' size='lg' alignSelf='flex-end'/>
+      <Heading mb='8' fontWeight='extrabold' size='2xl'>Todo Application</Heading>
+    </VStack>
   );
 }
 
