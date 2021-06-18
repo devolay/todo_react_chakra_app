1fc6e8038c0aa364188641842b799ba4b5d44ad6
dawsta006 <dawsta006@o365.student.utp.edu.pl>
2021-06-18 20:28:54 +0200

    added todo box (VStack)

--- src/App.js
+++ src/App.js
@@ -10,8 +10,20 @@ import { FaSun, FaMoon } from 'react-icons/fa'
 function App() {
   return (
     <VStack p={4}>
-      <IconButton icon={<FaSun></FaSun>} isRound='true' size='lg' alignSelf='flex-end'/>
-      <Heading mb='8' fontWeight='extrabold' size='2xl'>Todo Application</Heading>
+      <IconButton 
+        icon={<FaSun></FaSun>} 
+        isRound='true' size='lg' 
+        alignSelf='flex-end'/>
+      <Heading 
+        mb='8' 
+        fontWeight='extrabold' 
+        size='2xl' 
+        bgGradient="linear(to-r, green.800, green.500, green.800)"
+        bgClip='text'>
+          Todo Application
+      </Heading>
+      <TodoList />
+      <AddTodo />
     </VStack>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import { VStack, HStack, Text, IconButton, StackDivider } from '@chakra-ui/react'
+import { FaTrash } from 'react-icons/fa'
 
 function TodoList() {
+    const todos = [
+        {
+            id:1,
+            body: 'idz po chleb'
+        },
+        {
+            id:2,
+            body: 'zrób trening'
+        }
+    ]
+
     return (
-        <div>
-            
-        </div>
+        <VStack 
+            divider={<StackDivider />}
+            borderColor="gray.100"
+            borderWidth="2px"
+            p="4"
+            borderRadius="lg"
+            width="100%"
+            maxW={{base:'90vw', sm: '80vw', lg: '50vw', xl: '40vw'}}>
+            {todos.map(a => (
+                <HStack key={a.id}>
+                    <Text>{a.body}</Text>
+                    <IconButton icon={<FaTrash />} isRound='true'/>
+                </HStack>
+            ))}
+        </VStack>
     )
 }
 
