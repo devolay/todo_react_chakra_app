850b1685230e94da3adc6f7629d10713afd89eed
Dawid Stachowiak <dawid.stachowiak@o2.pl>
2021-06-19 11:31:40 +0200

    improved style

--- src/App.js
+++ src/App.js
@@ -1,29 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
 import { Heading } from '@chakra-ui/react'
 import TodoList from './components/TodoList'
 import AddTodo from './components/AddTodo'
 import { VStack, IconButton } from '@chakra-ui/react'
 import { FaSun, FaMoon } from 'react-icons/fa'
+import {useState, useEffect} from 'react'
 
 function App() {
+  const initialTodos = [
+    {
+        id:1,
+        body: 'idz po chleb'
+    },
+    {
+        id:2,
+        body: 'zrób trening'
+    }
+];
+
+  const[todos, setTodos] = useState(initialTodos);
+
+  function deleteTodo(id) {
+    const newTodos = todos.filter(todo=> {
+      return todo.id !== id
+    })
+    setTodos(newTodos)
+  }
+
+  function addTodo(todo){
+    setTodos(...todos, todo)
+  }
+
   return (
     <VStack p={4}>
       <IconButton 
         icon={<FaSun></FaSun>} 
         isRound='true' size='lg' 
         alignSelf='flex-end'/>
-      <Heading 
-        mb='8' 
+      <VStack 
+      spacing='8'
+      w='100%'>
+        <Heading 
         fontWeight='extrabold' 
-        size='2xl' 
-        bgGradient="linear(to-r, green.800, green.500, green.800)"
-        bgClip='text'>
+        size='2xl'
+        colorScheme='green'
+        marginBottom = '8px !important'
+        maxW={{base:'90vw', sm: '80vw', lg: '50vw', xl: '40vw'}}
+        >
           Todo Application
-      </Heading>
-      <TodoList />
-      <AddTodo />
+        </Heading>
+        <TodoList todos = {todos} deleteTodo = {deleteTodo} />
+      </VStack>
+      <AddTodo addTodo={addTodo} />
     </VStack>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import { VStack, HStack, Text, IconButton, StackDivider } from '@chakra-ui/react'
+import { VStack, HStack, Text, IconButton, StackDivider, Spacer, Badge } from '@chakra-ui/react'
 import { FaTrash } from 'react-icons/fa'
 
-function TodoList() {
-    const todos = [
-        {
-            id:1,
-            body: 'idz po chleb'
-        },
-        {
-            id:2,
-            body: 'zrób trening'
-        }
-    ]
+function TodoList({ todos, deleteTodo }) {
+
+    if(!todos.length){
+        return (
+            <Badge colorScheme='green'
+            p="4" 
+            m='4'
+            borderRadius='lg'>
+                No more Todos :)
+            </Badge>
+        )
+    }
 
     return (
         <VStack 
@@ -20,13 +21,16 @@ function TodoList() {
             borderColor="gray.100"
             borderWidth="2px"
             p="4"
+            mt = '8'
             borderRadius="lg"
             width="100%"
-            maxW={{base:'90vw', sm: '80vw', lg: '50vw', xl: '40vw'}}>
+            maxW={{base:'90vw', sm: '80vw', lg: '50vw', xl: '40vw'}}
+            align = "stretch">
             {todos.map(a => (
                 <HStack key={a.id}>
                     <Text>{a.body}</Text>
-                    <IconButton icon={<FaTrash />} isRound='true'/>
+                    <Spacer />
+                    <IconButton icon={<FaTrash />} isRound='true' onClick={() => deleteTodo(a.id)}/>
                 </HStack>
             ))}
         </VStack>
