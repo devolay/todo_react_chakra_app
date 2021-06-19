f74ab52f4b00ec86387efa84704cdc4bf51ac75f
Dawid Stachowiak <dawid.stachowiak@o2.pl>
2021-06-19 11:31:18 +0200

    added delete functionality and working on adding todos

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { HStack, Input, Button } from '@chakra-ui/react'
+import {useState} from 'react'
 
-function AddTodo() {
+
+function AddTodo({ addTodo }) {
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(content)
+        const todo = {
+            id: '',
+            body: content
+        };
+    }
+
+    const [content, setContent] = useState('');
+
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit}>
+            <HStack mt='8'>
+                <Input variant='filled' placeholder='Todo...' value={content} onChange={(e) => setContent(e.target.value)}/>
+                <Button colorScheme = 'green' px= "8" type='submit'>Add Todo</Button>
+            </HStack>
+        </form>
     )
 }
 
